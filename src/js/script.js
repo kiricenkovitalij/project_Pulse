d54c2d228d4edbd6f97e8a8d12196f32ac465620
Vitalii <kiricenkovitalij5@gmail.com>
2019-11-14 22:42:17 +0200

    Tabs and footer

--- src/js/script.js
+++ src/js/script.js
@@ -28,10 +28,29 @@ $(document).ready(function(){
             slidesToScroll: 1
           }
         }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
       ]
     
     });
+
+    $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+      $(this)
+        .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+        .closest('div.container').find('div.catalog__cards').removeClass('catalog__cards_active').eq($(this).index()).addClass('catalog__cards_active');
+    });
+
+    $('.catalog__link').each(function(i) {
+      $(this).on('click', function(e){
+        e.preventDefault();
+        $('.catalog__content').eq(i).toggleClass('catalog__content_active');
+        $('.catalog__list').eq(i).toggleClass('catalog__list_active')
+      })
+    })
+    
+    $('.catalog__back').each(function(i) {
+      $(this).on('click', function(e){
+        e.preventDefault();
+        $('.catalog__content').eq(i).toggleClass('catalog__content_active');
+        $('.catalog__list').eq(i).toggleClass('catalog__list_active')
+      })
+    })
   });
